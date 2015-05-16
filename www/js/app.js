4345fa2db71e73a7f6c324903b6cc339911272a3
Carl Hamlet <carl_hamlet@hotmail.com>
2015-05-16 09:41:25 +0200

    Captain!

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,6 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -17,3 +16,20 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('IndexCtrl', function($log, $scope) {
+  $log.debug("Captain - Start the engines please!");
+  $scope.captureAudio = function() {
+    var options = { limit: 3, duration: 10 };
+
+    $cordovaCapture.captureAudio(options).then(function(audioData) {
+      // Success! Audio data is here
+      $log.debug("yeah buddy!");
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+  }
+});
+
+
+
